5aca8d7d8315cb9224caac1279703b3dda1e0e87
Renato Neri Jr <renatonerijr@gmail.com>
2020-02-15 20:24:29 -0300

    Change Electron to NW.JS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './React-Pixelated.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        const process = window.require('process')
+        console.log(process.versions)
+        return (
+            <div className="App">
+                <div className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h2>Welcome to React</h2>
+                  <p> This project is running Node {process.versions['node']} and NW.JS {process.versions['nw']}</p>
+                  <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                  </p>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
